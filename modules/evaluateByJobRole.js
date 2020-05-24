b5f29287211000f48f027f2b1e0adf3414d3a513
Wookeun Song <wookeun.s@gmail.com>
2020-05-24 00:57:25 -0500

    a lot of progress

---
+++ modules/evaluateByJobRole.js
@@ -0,0 +1,13 @@
+const EVALUATION_CHAT = require("../db/evaluationChart.json")
+
+module.exports = (title, level, rateBySLandCS) => {
+    return 100
+}
+
+// 1 if NOT marketing
+// 2 bad
+// 3 mid
+// 4 PR and Comm
+
+// Additional
+// rateBySLandCS 
